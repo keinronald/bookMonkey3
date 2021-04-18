b124c8e07f2e2d408ed937761fd51e0c508e9f02
Ronald Keinberger <ronald.keinberger@vendevio.com>
2021-04-18 19:50:55 +0200

    implements http-interceptor

---
+++ src/app/shared/token-interceptor.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+  private authToken = '1234567890';
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const newRequest = request.clone({
+      setHeaders: {
+        Authorization: `Bearer ${this.authToken}`
+      }
+    });
+    return next.handle(newRequest);
+  }
+}
